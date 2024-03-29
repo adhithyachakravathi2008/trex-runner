caa601668db5507d737988153ee34f571ef330ad
adhithyachakravathi2008 <118976315+adhithyachakravathi2008@users.noreply.github.com>
2024-03-29 16:27:30 +0530

    Add files via upload

---
+++ Trex_Stage_0-main/sketch.js
@@ -0,0 +1,19 @@
+
+var trex ,trex_running;
+function preload(){
+  
+
+}
+
+function setup(){
+  createCanvas(600,200)
+  
+  //create a trex sprite
+ trex = createSprite(50,160,50,20);
+}
+
+function draw(){
+  background("white")
+  
+
+}
